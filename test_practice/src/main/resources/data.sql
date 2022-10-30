3d6f196bf8f09df46d8bc9d168a0f1f0958ff524
ibradi3 <ibrahimasory22@hotmail.com>
2022-10-30 22:33:23 +0100

    insert data to candy table

---
+++ test_practice/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO candy (name, price)
+VALUES ('Snickers', 1.50);
+INSERT INTO candy (name, price)
+VALUES ('Twix', 1.50);
+INSERT INTO candy (name, price)
+VALUES ('M&Ms', 1.50);
+INSERT INTO candy (name, price)
+VALUES ('Milky Way', 1.50);
+INSERT INTO candy (name, price)
+VALUES ('Kit Kat', 1.50);
+INSERT INTO candy (name, price)
+VALUES ('Reese''s', 1.50);
